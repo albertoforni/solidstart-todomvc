2f0879c7fca359b5436af5d75432b75510dd0b5e
Alberto Forni <me@albertoforni.com>
2024-01-10 21:56:00 +0100

    don't use unstorage

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,59 +1,87 @@
 "use server";
 
-import { createStorage } from "unstorage";
-import fsLiteDriver from "unstorage/drivers/fs-lite";
 import { Todo } from "~/types";
 
-// this uses file system driver for unstorage that works only on node.js
-// swap with the key value of your choice in your deployed environment
-const storage = createStorage({
-  driver: fsLiteDriver({
-    base: "./.data"
-  })
-});
-// storage.setItem("todos:data", []);
-// storage.setItem("todos:counter", 0);
+function createStorage() {
+  const storage = new Map<string, any>([["todos:counter", 0], ["todos:data", []]]);
+
+  return {
+    getItem(key: string) {
+      return storage.get(key);
+    },
+    getItems(keys: string[]) {
+      return keys.map((key) => storage.get(key));
+    },
+    setItem(key: string, value: any) {
+      storage.set(key, value);
+      return Promise.resolve();
+    }
+  };
+}
+
+const storage = createStorage();
 
 export async function getTodosFn() {
-  return await storage.getItem("todos:data") as Todo[];
+  return (await storage.getItem("todos:data")) as Todo[];
 }
 export async function addTodoFn(formData: FormData) {
   const title = formData.get("title") as string;
-  const [{value: todos}, {value: index}] = await storage.getItems(["todos:data", "todos:counter"]);
+  const [{ value: todos }, { value: index }] = await storage.getItems([
+    "todos:data",
+    "todos:counter",
+  ]);
 
   await Promise.all([
-    storage.setItem("todos:data", [...todos as Todo[], { id: index as number, title, completed: false }]),
-    storage.setItem("todos:counter", index as number + 1)
+    storage.setItem("todos:data", [
+      ...(todos as Todo[]),
+      { id: index as number, title, completed: false },
+    ]),
+    storage.setItem("todos:counter", (index as number) + 1),
   ]);
 }
 export async function removeTodoFn(id: number) {
-  const todos = await storage.getItem("todos:data") as Todo[];
-  await storage.setItem("todos:data", todos.filter(todo => todo.id !== id));
+  const todos = (await storage.getItem("todos:data")) as Todo[];
+  await storage.setItem(
+    "todos:data",
+    todos.filter((todo) => todo.id !== id)
+  );
 }
 export async function toggleTodoFn(id: number) {
-  const todos = await storage.getItem("todos:data") as Todo[];
-  await storage.setItem("todos:data", todos.map(todo => {
-    if (todo.id === id) {
-      todo.completed = !todo.completed;
-    }
-    return todo;
-  }));
+  const todos = (await storage.getItem("todos:data")) as Todo[];
+  await storage.setItem(
+    "todos:data",
+    todos.map((todo) => {
+      if (todo.id === id) {
+        todo.completed = !todo.completed;
+      }
+      return todo;
+    })
+  );
 }
 export async function editTodoFn(id: number, formData: FormData) {
   const title = String(formData.get("title"));
-  const todos = await storage.getItem("todos:data") as Todo[];
-  await storage.setItem("todos:data", todos.map(todo => {
-    if (todo.id === id) {
-      todo.title = title;
-    }
-    return todo;
-  }));
+  const todos = (await storage.getItem("todos:data")) as Todo[];
+  await storage.setItem(
+    "todos:data",
+    todos.map((todo) => {
+      if (todo.id === id) {
+        todo.title = title;
+      }
+      return todo;
+    })
+  );
 }
 export async function clearCompletedFn() {
-  const todos = await storage.getItem("todos:data") as Todo[];
-  await storage.setItem("todos:data", todos.filter(todo => !todo.completed));
+  const todos = (await storage.getItem("todos:data")) as Todo[];
+  await storage.setItem(
+    "todos:data",
+    todos.filter((todo) => !todo.completed)
+  );
 }
 export async function toggleAllFn(completed: boolean) {
-  const todos = await storage.getItem("todos:data") as Todo[];
-  await storage.setItem("todos:data", todos.map(todo => ({ ...todo, completed })));
+  const todos = (await storage.getItem("todos:data")) as Todo[];
+  await storage.setItem(
+    "todos:data",
+    todos.map((todo) => ({ ...todo, completed }))
+  );
 }
